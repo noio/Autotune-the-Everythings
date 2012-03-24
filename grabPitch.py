8fc052ad33e920941e8948de3c2a7209e6f2545a
=Tom Aizenberg <=tom.aizenberg@gmail.com>
2012-03-24 16:58:41 +0100

    key estimation using getKey(file)

---
+++ grabPitch.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+import sys
+from aubio.task import *
+from music21 import *
+
+def getKey(filename):
+
+  mode = "yin"
+  bufsize = 4096  
+  hopsize = float(bufsize) / 2
+
+  params = taskparams()
+  params.samplerate = float(sndfile(filename).samplerate())
+  params.hopsize    = int(float(bufsize) / 2)
+  params.bufsize    = bufsize
+  params.step       = params.samplerate/float(hopsize)
+  params.units      = "midi"
+  params.pitchmax   = 83
+
+  wplot,oplots,titles = [],[],[]
+
+  pitch = []
+  params.pitchmode  = mode
+  filetask = taskpitch(filename,params=params)
+  pitch = filetask.compute_all()
+
+  pitchlist = []
+  notes = stream.Stream()
+
+  i=0
+  for schnoof in pitch:
+    schnooi = int(schnoof)
+    if i%2==1 and schnooi!=-1:
+      pitchlist.append(schnooi)
+      n = note.Note()
+      n.pitch.midi = schnooi
+      n.quarterLength = 1
+      notes.append(n)
+      
+    i+=1
+
+  base = analysis.discrete.analyzeStream(notes, 'KrumhanslKessler').getScale().transpose(-36).pitches
+  pitches = []
+
+  for y in base:
+    n = note.Note()
+    n.pitch = y
+    if not n.pitch.frequency in pitches:
+      pitches.append(n.pitch.frequency)
+    for i in range(5):
+      n.pitch.midi += 12
+      if not n.pitch.frequency in pitches:
+        pitches.append(n.pitch.frequency)
+
+  pitches.sort()
+  return pitches
+
+print getKey("s/test.wav")
